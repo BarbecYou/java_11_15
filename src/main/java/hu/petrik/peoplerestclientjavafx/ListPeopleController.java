5717ebbaeca7cd73aa8fa4a893ab8d8d5e74091b
Füredi Dániel <daniel.furedi03@gmail.com>
2022-11-15 16:00:43 +0100

    adatok betöltése és megjelenítése, hibakezelés

--- src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
+++ src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
@@ -1,11 +1,11 @@
 package hu.petrik.peoplerestclientjavafx;
 
+import com.google.gson.Gson;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
 
@@ -27,26 +27,46 @@ public class ListPeopleController {
     private TableColumn<Person, Integer> ageCol;
 
     @FXML
-    private void initialize(){
-        try {
-            Response response = RequestHandler.get(App.BASE_URL);
-            String content = response.getContent();
-            System.out.println(content);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private void initialize() {
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        ageCol.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+        Platform.runLater(() -> {
+            try {
+                Response response = RequestHandler.get(App.BASE_URL);
+                String content = response.getContent();
+                Gson converter = new Gson();
+                Person[] people = converter.fromJson(content, Person[].class);
+                for (Person person : people) {
+                    peopleTable.getItems().add(person);
+                }
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("ERROR");
+                alert.setHeaderText("Couldn't get data from server");
+                alert.setContentText(e.getMessage());
+
+                alert.showAndWait();
+                Platform.exit();
+
+            }
+        });
     }
 
     @FXML
-    public void insertClick(ActionEvent actionEvent){
+    public void insertClick(ActionEvent actionEvent) {
 
     }
+
     @FXML
-    public void updateClick(ActionEvent actionEvent){
+    public void updateClick(ActionEvent actionEvent) {
 
     }
+
     @FXML
-    public void deleteClick(ActionEvent actionEvent){
+    public void deleteClick(ActionEvent actionEvent) {
 
     }
 
